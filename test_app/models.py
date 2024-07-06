098052537a2e9602102ca2da969216f1e024a91c
admin <320996299@qq.com>
2024-07-06 11:53:19 +0800

    配置数据库，设置模型并迁移

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    name=models.CharField(max_length=20)
+
+
+class PeopleInfo(models.Model):
+    name=models.CharField(max_length=20)
+    gender=models.BooleanField()
+
+    # 外键约束
+    book=models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+
