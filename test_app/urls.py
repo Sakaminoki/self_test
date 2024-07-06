bb234ff4dd4af044b5c2450c8cda5748e74ac674
admin <320996299@qq.com>
2024-07-06 15:19:50 +0800

    设置url，设置index页面，返回请求方法

---
+++ test_app/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+
+from test_app.views import index
+
+urlpatterns = [
+    path('index/',index),
+]
--- test_app/views.py
+++ test_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
 
 # Create your views here.
+def index(request):
+
+    requestMethod=request.method
+    return HttpResponse(requestMethod)
