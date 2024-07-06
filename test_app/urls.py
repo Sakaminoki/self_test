77489db84e496413135f3527d140dc8207d58a3e
admin <320996299@qq.com>
2024-07-06 15:54:46 +0800

    设置模板处理数据

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from test_app.views import index
+from test_app.views import index,template_test
 
 urlpatterns = [
     path('index/',index),
+    path('template',template_test),
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -6,3 +6,13 @@ def index(request):
 
     requestMethod=request.method
     return HttpResponse(requestMethod)
+
+
+def template_test(request):
+    # 设置字典内容
+    context={
+        'name':'tom',
+        'age':'20',
+    }
+
+    return render(request,'test_app/index.html',context)
